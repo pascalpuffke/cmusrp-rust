b251def936d3622e34fcf9967b952a89d15b2436
Pascal Puffke <pascalpuffke@protonmail.ch>
2021-03-21 03:38:04 +0100

    switch to structopt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::fmt::Debug;
 use std::{thread::sleep, time::Duration};
 
-use clap::{App, Arg};
 use discord_rpc_client::Client;
+use structopt::StructOpt;
 
 use parser::Tag;
 
@@ -10,21 +10,28 @@ mod parser;
 mod shell;
 
 // TODO: add support for custom format strings in rp display
-#[derive(Debug)]
+#[derive(Debug, StructOpt)]
 struct Arguments {
+    #[structopt(
+        short,
+        long,
+        help = "Disables rich presence and displays current status on stdout"
+    )]
     debug: bool,
+    #[structopt(
+        short,
+        long,
+        default_value = "1000",
+        help = "Polling interval in which the program grabs the current status, in milliseconds"
+    )]
     interval: u64,
 }
 
 const ID: u64 = 718109162923360327;
-const VERSION: &str = "1.1.0";
+const VERSION: &str = "1.2.0";
 
 fn main() {
-    let mut args = Arguments {
-        debug: false,
-        interval: 1000,
-    };
-    parse_args(&mut args);
+    let args = Arguments::from_args();
 
     let mut client = Client::new(ID);
 
@@ -36,48 +43,17 @@ fn main() {
     main_loop(client, args);
 }
 
-fn parse_args(args_struct: &mut Arguments) {
-    let args = App::new("cmusrp-rust")
-        .version(VERSION)
-        .author("Pascal Puffke <pascal@pascalpuffke.de>")
-        .about("External Discord Rich Presence provider for cmus")
-        .arg(
-            Arg::with_name("debug")
-                .short("d")
-                .long("debug")
-                .help("Disables rich presence and displays current status on stdout")
-        )
-        .arg(
-            Arg::with_name("interval")
-                .short("i")
-                .long("interval")
-                .help("Polling interval in which the program grabs the current status, in milliseconds")
-                .takes_value(true)
-        )
-        .get_matches();
-
-    if args.is_present("interval") {
-        let value = args
-            .value_of("interval")
-            .unwrap()
-            .parse::<u64>()
-            .unwrap_or_else(|e| panic!("Unable to parse integer from string: {}", e));
-
-        args_struct.interval = value;
-    }
-
-    args_struct.debug = args.is_present("debug");
-}
-
 fn main_loop(mut client: Client, args_struct: Arguments) {
     loop {
         let remote = shell::get_stdout("cmus-remote", "-Q").unwrap_or(String::new());
 
         if parser::is_playing(&remote) {
-            let artist = get_tag(Tag::Artist, &remote);
-            let title = get_tag(Tag::Title, &remote);
-            let album = get_tag(Tag::Album, &remote);
-            let date = get_tag(Tag::Date, &remote);
+            let (artist, title, album, date) = (
+                get_tag(Tag::Artist, &remote),
+                get_tag(Tag::Title, &remote),
+                get_tag(Tag::Album, &remote),
+                get_tag(Tag::Date, &remote),
+            );
 
             if args_struct.debug {
                 println!("{}\n{} - {} ({})\n", title, artist, album, date);
